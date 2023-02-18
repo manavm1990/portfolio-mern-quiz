33890cbc7c2d7fd783af3972f74522f968627eef
Manav Misra <manavm@visionwebsoft.com>
2023-02-17 21:50:43 -0600

    Include error messages in Zod

--- src/user/user.ts
+++ src/user/user.ts
@@ -3,12 +3,19 @@ import { z } from "zod";
 
 export const User = z.object({
   _id: z.instanceof(ObjectId),
-  email: z.string().email(),
-  mobile: z.string().regex(/^\d{10}$/),
-
-  // Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, and one number.
-  password: z.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/),
-  username: z.string().trim().length(3),
+  email: z.string().email({
+    message: "Please enter a valid email address.",
+  }),
+  mobile: z.string().regex(/^\d{10}$/, {
+    message: "Please enter a valid 10-digit mobile number.",
+  }),
+  password: z.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, {
+    message:
+      "Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, and one number.",
+  }),
+  username: z.string().trim().length(3, {
+    message: "Username must be at least 3 characters long.",
+  }),
 });
 
 export type User = z.infer<typeof User>;
