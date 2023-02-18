33411e0b9f79e8175f1f8a750772a010130affcc
Manav Misra <manavm@visionwebsoft.com>
2023-02-17 21:45:14 -0600

    Solidify Zod User

--- src/user/user.ts
+++ src/user/user.ts
@@ -1,11 +1,14 @@
+import { ObjectId } from "mongodb";
 import { z } from "zod";
 
 export const User = z.object({
-  _id: z.string(),
-  email: z.string(),
-  mobile: z.string(),
-  password: z.string(),
-  username: z.string(),
+  _id: z.instanceof(ObjectId),
+  email: z.string().email(),
+  mobile: z.string().regex(/^\d{10}$/),
+
+  // Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, and one number.
+  password: z.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/),
+  username: z.string().trim().length(3),
 });
 
 export type User = z.infer<typeof User>;
