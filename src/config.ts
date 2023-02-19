af7d015095a632ad75d0b5728d1bae9494792324
Manav Misra <manavm@visionwebsoft.com>
2023-02-19 15:28:58 -0600

    :wrench: 🍪

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default {
+  cookie: {
+    maxAge: Number(process.env.COOKIE_MAX_AGE) || 1000 * 60 * 60 * 24 * 7, // 7 days
+    name: process.env.COOKIE_NAME || "qid",
+    secret: process.env.COOKIE_SECRET!,
+  },
   error: {
     invalidLogin: "Invalid username or password.",
   },
