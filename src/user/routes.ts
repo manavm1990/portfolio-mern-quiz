0ca9127f5ce648d0c445725505d8cd466af66a54
Manav Misra <manavm@visionwebsoft.com>
2023-02-19 14:48:18 -0600

    feat: :sparkles: register user
    Includes error handling for dupes 🥅.

--- src/user/routes.ts
+++ src/user/routes.ts
@@ -1,16 +1,19 @@
 import { Router } from "express";
+import validateBody from "../middleware/validate-body.js";
+import userController from "./controller.js";
 import { ZUser } from "./user.js";
 
 const router = Router();
 
-router.post("/register", async (req, res) => {
-  try {
-    ZUser.parse(req.body);
-  } catch (e) {
-    return res.status(400).json(e);
-  }
+router.post("/register", validateBody(ZUser), async (req, res) => {
+  const jwt = await userController.create(req.body).catch((err) => {
+    if (err.code === "P2002")
+      return res.status(409).json({ message: "User already exists❗" });
 
-  return res.send("User created.");
+    return res.status(500).json(err);
+  });
+
+  res.status(201).json(jwt);
 });
 
 export default router;
